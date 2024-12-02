6ce58e7f2aaf89c08666469f2dc2b6ee943ef615
enhbayasgalan <enhuush0109@gmail.com>
2024-12-02 19:06:28 +0800

    part

---
+++ index.js
@@ -0,0 +1,72 @@
+// gurvaljin premitr // 
+const abl = "12";
+const bal = "30";
+const cal = "18";
+console.log(abl * bal * cal)
+
+// toirgiin talbai //
+const a = "14";
+const b = "10";
+const c = "6";
+console.log( a + b + c)
+
+// m=y2-y1/x2-x1 //
+const y2 = 10
+const y1 = 15
+const x1 = 4
+const x2 = 6
+console.log( y2 - x2 + x1 - y2)
+
+// 15MB //
+const q1 = 5;
+const q2 = 15;
+console.log(q2 - q1)
+
+// side2 side3 side1 //
+const side1 = 14 
+const side2 = 8 
+const side3 = 4
+console.log(side1 * side2 + side3) 
+
+// цельс //
+const celius = "12"
+console.log(12 * 1.8)+32 
+
+// hoir bag //
+const A  = 96 
+const B = 81 
+console.log(A - B)
+
+// 15. //
+const huvs1 = 25
+const huvs2 = 5
+console.log( huvs1 - huvs2)
+
+const huv1 = 25
+const hus2 = 5
+console.log( huvs1 + huvs2)
+
+const hvs1 = 25
+const hvs2 = 5
+console.log( huvs1 * huvs2)
+
+const hs1 = 25
+const hs2 = 5
+console.log( huvs1 / huvs2)
+
+// cm inch // 
+const inch = 1
+const inch2 = 2.5
+console.log(2.5 * 1)
+
+// duguin radius //
+const r = 12
+console.log(r - 180 / 2)
+
+const r2 = 12
+console.log(r * 180 / 2)
+
+// 3 orontoi //
+const too=123;
+const ehnii=parseInt(too/1000)
+console.log("ehnii too" +ehnii);
